808d1c331d21744c22ceb99aef2c051fff16e400
Yuduo Zhao <yuduozhao@gmail.com>
2021-06-26 08:10:28 -0700

    feat: Add sorting to FDR table

--- src/components/Fixtures.tsx
+++ src/components/Fixtures.tsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Layout, Table } from 'antd';
 import { StoreState } from '../reducers';
 import { FixtureData, Fixture, Season } from '../types';
-import { getTeamNames } from '../data';
+import { getTeamFullNames, getTeamNames } from '../data';
 
 const { Content } = Layout;
 
@@ -12,9 +12,18 @@ interface Props {
     gameweek: number;
 }
 
+interface Row {
+    [key: string]: string | Fixture[];
+};
+
+function assertIsArrayOfFixtures(obj: unknown): asserts obj is Fixture[] {
+    if (Array.isArray(obj) && (obj.length === 0 || obj[0].opponent != null)) return;
+    else throw new Error('Input must be a string!');
+}
+
 const renderCell = (gwFixtures: Fixture[]) => {
     const TEAM_NAMES = getTeamNames(Season.S2018_2019); // TODO: Remove hardcoded season
-    
+
     if (gwFixtures.length === 0) {
         return <div className={'fdr fdr-0'}></div>;
     }
@@ -30,7 +39,7 @@ const renderCell = (gwFixtures: Fixture[]) => {
         {gwFixtures.map(fixture => {
             const team = TEAM_NAMES[fixture.opponent];
             const location = fixture.isHome ? '(H)' : '(A)';
-            return <div className={fdrClass + ' fdr-sub fdr-' + fixture.difficulty}>
+            return <div className={fdrClass + ' fdr-sub fdr-' + fixture.difficulty} key={fixture.opponent}>
                 {team} <br/> {location}
             </div>
         })}
@@ -38,31 +47,47 @@ const renderCell = (gwFixtures: Fixture[]) => {
 };
 
 const generateFDRTable = (fixtures: FixtureData, gameweek: number) => {
-    const TEAM_NAMES = getTeamNames(Season.S2018_2019); // TODO: Remove hardcoded season
+    const TEAM_FULL_NAMES = getTeamFullNames(Season.S2018_2019); // TODO: Remove hardcoded season
     const columns: object[] = [
         {
             title: 'Team',
             dataIndex: 'team',
             key: 'team',
             fixed: 'left',
-            width: 50
+            width: 70,
+            sorter: {
+                compare: (a: Row, b: Row) => a.team < b.team ? -1 : a.team > b.team ? 1 : 0
+            }
         }
     ];
     const maxGW = fixtures[0].length;
     for (let i = gameweek; i < maxGW; i++) {
-        const title = 'GW' + (i + 1);
+        const gwTitle = 'GW' + (i + 1);
         columns.push({
-            title,
-            dataIndex: title,
-            key: title,
+            title: gwTitle,
+            dataIndex: gwTitle,
+            key: gwTitle,
             width: 50,
-            render: renderCell
+            render: renderCell,
+            sorter: {
+                compare: (a: Row, b: Row) => {
+                    const aGW = a[gwTitle];
+                    const bGW = b[gwTitle];
+                    const result = aGW.length - bGW.length;
+                    if (result !== 0 || aGW.length === 0) return result;
+                    assertIsArrayOfFixtures(aGW);
+                    assertIsArrayOfFixtures(bGW);
+                    const aDiffiSum = aGW.reduce((acc, val) => acc + val.difficulty, 0);
+                    const bDiffiSum = bGW.reduce((acc, val) => acc + val.difficulty, 0);
+                    return bDiffiSum - aDiffiSum;
+                }
+            }
         });
     }
     const data: object[] = [];
     fixtures.forEach((teamGWs, teamIdx) => {
-        const row: { [key: string]: string | Fixture[]; } = {
-            team: TEAM_NAMES[teamIdx]
+        const row: Row = {
+            team: TEAM_FULL_NAMES[teamIdx]
         };
         teamGWs.forEach((teamGW, teamGWIdx) => {
             if (teamGWIdx < gameweek) return;
@@ -70,7 +95,8 @@ const generateFDRTable = (fixtures: FixtureData, gameweek: number) => {
         });
         data.push(row);
     });
-    return <Table dataSource={data} columns={columns} pagination={false} scroll={{ x: 90 * (maxGW - gameweek + 1) }} className='custom-table' />;
+    const tableWidth = 90 * (maxGW - gameweek + 1);
+    return <Table dataSource={data} columns={columns} pagination={false} scroll={{ x: tableWidth }} className='custom-table' />;
 }
 
 const _Content = ({ fixtures, gameweek }: Props) => (
