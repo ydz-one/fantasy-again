22e74227563e55f453f5a34bccc6bc3dbec9cd50
Yuduo Zhao <yuduozhao@gmail.com>
2022-01-22 09:46:43 -0800

    chore: Add homepage to package.json and update BrowserRouter to HashRouter in order to successfully build project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './components/App';
 import { createStore } from 'redux';
@@ -14,9 +14,9 @@ const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
