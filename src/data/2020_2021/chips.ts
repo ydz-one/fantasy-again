e055d88ed2bfd4b1f1bbaafafedc6257da128721
Yuduo Zhao <yuduozhao@gmail.com>
2022-02-11 13:29:35 -0800

    fix: Fix second wild card date

--- src/data/2020_2021/chips.ts
+++ src/data/2020_2021/chips.ts
@@ -1,7 +1,7 @@
 import { Chip, ChipCount } from '../../types';
 
 export const chipCountUpdateFns = {
-    18: function (chipCount: ChipCount) {
+    16: function (chipCount: ChipCount) {
         return {
             ...chipCount,
             [Chip.WILD_CARD]: 1,
