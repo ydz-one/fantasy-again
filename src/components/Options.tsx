070f834bcadfc44fed5dd3e858a819ac63150590
Yuduo Zhao <yuduozhao@gmail.com>
2022-02-13 08:35:18 -0800

    feat: Add About button and modal

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -12,21 +12,30 @@ interface Props {
 
 const _Options = ({ resetGameState, resetDataState }: Props) => {
     const history = useHistory();
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isResetGameModalVisible, setIsResetGameModalVisible] = useState(false);
+    const [isAboutModalVisible, setIsAboutModalVisible] = useState(false);
 
     const handleConfirmRestartGame = () => {
-        setIsModalVisible(true);
+        setIsResetGameModalVisible(true);
     };
 
-    const handleOk = () => {
+    const handleResetGameOk = () => {
         resetGameState();
         resetDataState();
-        setIsModalVisible(false);
+        setIsResetGameModalVisible(false);
         history.push('/squadselection');
     };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
+    const handleResetGameCancel = () => {
+        setIsResetGameModalVisible(false);
+    };
+
+    const handleShowAboutModal = () => {
+        setIsAboutModalVisible(true);
+    };
+
+    const handleAboutCancel = () => {
+        setIsAboutModalVisible(false);
     };
 
     return (
@@ -34,23 +43,68 @@ const _Options = ({ resetGameState, resetDataState }: Props) => {
             <div className="site-layout-background">
                 <div className="page-title">Options</div>
                 <div className="flex-center-item">
-                    <Button
-                        size="large"
-                        className="top-btn-large top-btn-large-left"
-                        onClick={handleConfirmRestartGame}
+                    <div>
+                        <Button size="large" className="top-btn-large options-btn" onClick={handleShowAboutModal}>
+                            About
+                        </Button>
+                        <Button size="large" className="top-btn-large options-btn" onClick={handleConfirmRestartGame}>
+                            Restart Game
+                        </Button>
+                    </div>
+                    <Modal
+                        title="About This Game"
+                        visible={isAboutModalVisible}
+                        onCancel={handleAboutCancel}
+                        footer={[]}
                     >
-                        Restart Game
-                    </Button>
+                        <p>
+                            This is a free and open source game made with TypeScript, React, and Redux. It is intended
+                            as a way for FPL players to play out "what if" scenarios and to try out different transfer
+                            and chip strategies. It can also be used as a tutorial for beginner FPL players to prepare
+                            for the next season.
+                        </p>
+                        <p>
+                            You can find the source code{' '}
+                            <a
+                                href="https://github.com/ydz-one/fantasy-again"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                here
+                            </a>
+                            . Please raise an issue in that repo if you find a bug.
+                        </p>
+                        <p>
+                            If you are studying my code to learn programming and have a specific question about it, feel
+                            free to reach out to me and I will answer your question if I am available. My contact
+                            details are on{' '}
+                            <a href="https://ydz.one/" target="_blank" rel="noopener noreferrer">
+                                my website
+                            </a>
+                            .
+                        </p>
+                        <p>
+                            Special thanks to vaastav for providing the data used in this game in his{' '}
+                            <a
+                                href="https://github.com/vaastav/Fantasy-Premier-League"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                FPL dataset repo
+                            </a>
+                            .
+                        </p>
+                    </Modal>
                     <Modal
                         title="Confirm Restart Game"
-                        visible={isModalVisible}
-                        onOk={handleOk}
-                        onCancel={handleCancel}
+                        visible={isResetGameModalVisible}
+                        onOk={handleResetGameOk}
+                        onCancel={handleResetGameCancel}
                         footer={[
-                            <Button key="back" onClick={handleOk}>
+                            <Button key="back" onClick={handleResetGameOk}>
                                 Yes
                             </Button>,
-                            <Button key="submit" type="primary" onClick={handleCancel}>
+                            <Button key="submit" type="primary" onClick={handleResetGameCancel}>
                                 No
                             </Button>,
                         ]}
