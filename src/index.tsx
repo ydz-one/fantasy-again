9ed1ed97bed085d72bdaf7fb223413da392aab6f
Yuduo Zhao <yuduozhao@gmail.com>
2025-03-19 08:02:43 -0700

    Update primary theme color

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import './index.css';
-import { App } from './components/App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { reducers } from './reducers';
-import * as serviceWorker from './serviceWorker';
-import { applyMiddleware } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import "./index.css";
+import { App } from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducers } from "./reducers";
+import * as serviceWorker from "./serviceWorker";
+import { applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: "#6b71ff",
+  },
+});
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
+  <ConfigProvider>
     <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </Provider>,
-    document.getElementById('root')
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </ConfigProvider>,
+  document.getElementById("root")
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
